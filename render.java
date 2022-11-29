232695dd6559680b8905c26bf1e1938b7c5c1061
PAVLO SYVYK <116568036+Tokimitsu11@users.noreply.github.com>
2022-11-29 20:48:10 +0100

    Create render.java

---
+++ render.java
@@ -0,0 +1,25 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        byte a = 3;
+        short b = 23;
+        int c = 155;
+        long d = 435678643l;
+        double e = 22.22;
+        float f = 3;
+        boolean g = true;
+        String h = "$";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
